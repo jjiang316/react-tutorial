92873720c34f13f6136f03cb988863e394419ea0
jjiang316 <jjiang316@gmail.com>
2022-04-04 13:39:23 -0500

    read from a database

---
+++ src/utilities/firebase.js
@@ -0,0 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref } from 'firebase/database';
+import  { useState, useEffect } from 'react';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXIasCTzA2WRuAg9ad6PXwCpz100F2bi8",
+  authDomain: "react-tutorial-d6522.firebaseapp.com",
+  databaseURL: "https://react-tutorial-d6522-default-rtdb.firebaseio.com",
+  projectId: "react-tutorial-d6522",
+  storageBucket: "react-tutorial-d6522.appspot.com",
+  messagingSenderId: "84174535233",
+  appId: "1:84174535233:web:022afcecb8a9d550f7a4c0",
+  measurementId: "G-CTGLZPDSGW"
+};
+
+// Initialize Firebase
+const firebase  = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+  };
